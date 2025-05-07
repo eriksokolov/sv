1f20835d5251b9a359ce34545cf607a47488eae4
eriksokolov <erikstankevich@yandex.com>
2025-05-07 21:46:26 +0200

    Create an odin project - create string, and print out last letter

---
+++ src/routes/odin/lastletter/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+function lastletter(str) {
+    return str.charAt(str.length -1);
+}
+
+let list = ['foo', 'bar', 'baz'];
+let i = 0;
+let rIndex;
+
+while (i < 1) {
+    rIndex= Math.floor(Math.random()* list.length);
+    i = i + 1;
+}
+
+
+let foo = list[rIndex];
+let funky = lastletter(foo);
+console.log(foo)
+console.log(funky)
+
+
+</script>
+
+<style>
+    h1 {
+        color: purple;
+    }
+</style>
+
+<h1>Hello name!</h1>
