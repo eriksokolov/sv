1539defd63d0e5fa46b481246a4322a711036029
eriksokolov <erikstankevich@yandex.com>
2025-05-07 20:01:05 +0200

    use .#c and .#f as placeholders for f and c and create temperture converter

---
+++ src/routes/temperature/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+   class Temperature {
+    #c = $state(0)
+    #f = $state(0)
+
+    get c() {
+        return this.#c
+    }
+
+    set c(c) {
+    this.#c = c
+    this.#c = c * (9/5) + 32
+    }
+
+    get f() {
+        return this.#f
+    }
+
+    set f(f) {
+        this.#f = f
+        this.#c = (f - 32) * (5/9)
+    }
+
+
+} 
+
+const temperature = new Temperature()
+</script>
+
+<div>
+    <input type="number" bind:value={temperature.c} /> Celsius =
+    <input type="number" bind:value={temperature.f} /> Fahrenheit
+</div>
+
+
+<style>
+    input[type="number"] {
+        max-width: 10ch;
+    }
+</style>
