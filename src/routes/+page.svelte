64f3674c4810412f7a8a1561d3ee456174308859
eriksokolov <erikstankevich@yandex.com>
2025-05-07 17:52:16 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    let count = $state(0)
+</script>
+
+<div class="flex-gap">
+    <input type="text" bind:value={count}>
+    <button onclick={() => count = count + 1}>Count</button>
+</div>
