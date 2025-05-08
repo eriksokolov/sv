573e3afbbe2e1f36eb5a59e4bc93847f1f0a1f1e
eriksokolov <erikstankevich@yandex.com>
2025-05-08 20:53:55 +0200

    template for prompt() in svelte

---
+++ src/routes/odin/rockpaperscissors/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+
+    function getComputerChoice(){
+        /*random number, 0,1,2*/
+        let pc = Math.floor(Math.random() * 3);
+        console.log(pc);
+        return(pc);
+    }
+    let result = getComputerChoice();
+    console.log(result);
+
+
+</script>
