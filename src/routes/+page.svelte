88ee745e9d41e173657d993285b65e9d679763c8
eriksokolov <erikstankevich@yandex.com>
2025-05-12 22:42:44 +0200

    change index page, make page invert on hover, except image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,54 @@
-<script lang="ts">
-    let count = $state(0)
+<script>
+
+function invertion() {
+    console.log("invert")
+    document.body.style.filter = "invert(1)";
+    document.body.style.backgroundColor = "black";
+    document.getElementById("malevich").style.filter = "invert(1)";
+}
+
 </script>
 
-<div class="flex-gap">
-    <input type="text" bind:value={count}>
-    <button onclick={() => count = count + 1}>Count</button>
+<div id="title">Website</div>
+
+<br>
+<hr>
+<br>
+
+<div id="uptext">
+    приветь, я Эрик, я немец, люблю выучить русский, люблю читать, и хочу стать программистам.
 </div>
 
-<a href="./temperature/">temperature converter</a>
+<br>
+<hr>
+<br>
+
+<div id="malevichCon" >
+    <img id="malevich" src=/malevich.jpg alt="malevich's square" on:mouseover={invertion}>
+</div>
+
+<br>
+<hr>
+<br>
+
+<style>
+
+    #title{
+        font-size: 30px;
+        text-align: center;
+    }
+    #uptext{
+        font-size: 20px;
+        text-align: center;
+    }
+    #malevichCon{
+        text-align: center;
+    }
+    #malevich{
+        height: 100px;
+        width: 100px;
+    }
+
+
+</style>
+
---
+++ src/routes/state/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    let count = $state(0)
+</script>
+
+<div class="flex-gap">
+    <button onclick={() => count = count + 1}>{count}</button>
+</div>
