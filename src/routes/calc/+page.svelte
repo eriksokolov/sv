b9250c5c71184809ef8f9b37a7077db576ee3331
eriksokolov <erikstankevich@yandex.com>
2025-05-17 07:13:57 +0200

    add reset button, remove additon function, add +-*/ buttons and change <p> concatination

---
+++ src/routes/calc/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+    let tiles = [1,2,3,4,5,6,7,8,9];
+    let selected = [];
+    let result = 0;
+    let tools = [
+        (a,b) => a+b,
+        (a,b) => a-b,
+        (a,b) => a*b,
+        (a,b) => a/b
+    ]
+    let toolsign = [
+        "+",
+        "-",
+        "*",
+        "/",
+    ]
+
+
+</script>
+<style>
+.key {
+    background-color:blueviolet;
+    border: 1px solid white;
+}
+.reset{
+    background-color:forestgreen;
+}
+.tools{
+    background-color:orange;
+}
+</style>
+
+{#each tiles as key}
+    <div class="key" on:click={() => selected = [...selected, key]}>
+        {key}
+    </div>
+{/each}
+<hr>
+<p>1. {selected.join()}</p>
+<p>2. {result}</p>
+<b class="reset" on:click={() => {
+    selected = [];
+    result = 0;
+}}>esc</b>
+{#each tools as tool, i}
+<div>
+    <b class="tools"
+        on:click={tools[i]} 
+    >{toolsign[i]}</b>
+</div>
+{/each}
