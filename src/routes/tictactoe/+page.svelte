4626ef540e8c444262c8e40cf93f8148cce0e48d
eriksokolov <erikstankevich@yandex.com>
2025-05-20 08:23:09 +0200

    make player choice possible

--- src/routes/tictactoe/+page.svelte
+++ src/routes/tictactoe/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     let grids = [1,2,3,4,5,6,7,8,9];
+    let marks = ["", "", "", "", "", "", "", ""];
+    let lamp = 0;
 </script>
 
 <style>
@@ -32,11 +34,14 @@
 </style>
 
 <div class = "grid">
-    {#each grids as grid}
-        <div> 
+    {#each grids as grid, i}
+        <div on:click={() =>{ marks[i] = "X"; console.log(marks[i]); lamp = 1;}}> 
             <svg width="100px" height="100px" >
                 <rect width="100px" height="100px" stroke="orange" stroke-width="1">
                 </rect>
+                <text class= svgtext x= "50px" y="50" text-anchor="middle" dominant-baseline="middle" font-size="20" fill="white">
+                    {marks[i]}                    
+                </text>
             </svg>
         </div>
     {/each}
