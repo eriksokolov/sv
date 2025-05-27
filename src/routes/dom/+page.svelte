bd05ce407b5a8f1ad2bfe391087bbd52d213a2ab
eriksokolov <erikstankevich@yandex.com>
2025-05-27 11:25:51 +0200

    practice js dom manipulation

---
+++ src/routes/dom/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    import {onMount, onDestroy} from 'svelte';
+
+    const funion = () => {
+        console.log('foo');
+        let head = document.querySelector('head');
+        let newTitle = document.createElement('title');
+        newTitle.textContent = "foo";
+        head.appendChild(newTitle);
+    }
+
+    onMount(() => {
+        const btn = document.getElementById('btn');
+        btn.addEventListener('click', funion);
+    
+        onDestroy(() => {
+            btn.removeEventListener('click', funion);
+        });
+    });
+</script>
+<style>
+    :global(body){
+        background-color: black;
+        color: #fff;
+        font-family: sans-serif;
+    }
+</style>
+
+<div id="btn">X</div>
+
---
+++ src/routes/weather/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  let imageUrl = '';
+
+  // Загружаем гифку при монтировании компонента
+  onMount(async () => {
+    const res = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=miVNpodeyiDJ9JrB9mRbd2OJZbtriYF1&s=cats', { mode: 'cors' });
+    const data = await res.json();
+    imageUrl = data.data.images.original.url;
+  });
+
+  import { onMount } from 'svelte';
+</script>
+<style>
+    :global(body){
+        background-color: black;
+        display: flex;
+        justify-content: center;
+        padding: 2rem;
+    }
+</style>
+
+<h1>Hello</h1>
+{#if imageUrl}
+  <img src="{imageUrl}" alt="Cat gif">
+{:else}
+  <p>Loading...</p>
+{/if}
