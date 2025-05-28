fb4808552df3458da062e3530d2b4b88451892f9
eriksokolov <erikstankevich@yandex.com>
2025-05-28 18:30:49 +0200

    check out grid, convenient way to align boxes like divs

---
+++ src/routes/flex/+page.svelte
@@ -0,0 +1,58 @@
+<style>
+    :global(body){
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        font-family: sans-serif;
+        font-size: 16px;
+        line-height: 1.5;
+        color: #333;
+        background: #a1c3ff;
+    }
+    .upContainer {
+        max-width: 1100px;
+        margin: 0 auto;
+        padding: 0 30px;
+        background: #3474e6;
+        color: #fff;
+        height: 60px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .upList {
+        display:flex;
+
+    }
+    .logo{
+        font-size: x-large;
+        font-weight: bold;
+    }
+    a {
+        color: #fff;
+        text-decoration: none;
+        font-size: 18px;
+        font-weight: bold;
+    }
+    li {
+        margin-left: 20px;
+        text-decoration: none;
+
+    }
+</style> 
+
+<div class="upContainer">
+    <div class="logo">Logo
+    </div>
+    <ul class="upList">
+        <li>
+            <a href="./">Home</a>
+        </li>
+        <li>
+            <a href="x">About</a>
+        </li>
+        <li>
+            <a href="x">Contact</a>
+        </li>
+    </ul>
+</div>
---
+++ src/routes/grid/+page.svelte
@@ -0,0 +1,36 @@
+<style>
+    .container{
+        max-width: 960px;
+        margin: 100px auto;
+        padding: 10px;
+        display:grid;
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+    .item{
+        background:steelblue;
+        color: #fff;
+        font-size: 20px;
+        padding: 20px;
+        border: skyblue 1px solid;
+    }
+    .item:nth-of-type(2){
+        height: 100px;
+        width: 100px;
+        justify-self: center;
+    }
+@media (max-width: 900px){
+    .container {
+        grid-template-columns: 1fr;
+    }
+}
+</style>
+<div class="container">
+    <div class="item">item</div>
+    <div class="item">item</div>
+    <div class="item">item</div>
+    <div class="item">item</div>
+    <div class="item">item</div>
+    <div class="item">item</div>
+    <div class="item">item</div>
+    <div class="item">item</div>
+</div>
