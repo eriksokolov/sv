8f09363b738a09c4a49e03130366196276d67c8d
eriksokolov <erikstankevich@yandex.com>
2025-05-29 14:54:55 +0200

    drawing yellow and black square

---
+++ src/routes/supermatism/+page.svelte
@@ -0,0 +1,20 @@
+<style>
+    :global(body){
+        height: 100vh;
+        display:grid;
+        justify-content:center;
+        align-content: center;
+        background-color: black;
+    }
+    .canvas{
+        height: 30rem;
+        width: 30rem;
+        background: #f4f4f4;
+    }
+</style>
+<div class="canvas">
+  <svg viewBox="0 0 1 1">
+    <polygon points="0.5,0.9 0.5,0.7 0.7,0.7 0.7,0.9" fill="orange"></polygon>
+    <polygon points="0.6,0.6 0.6,0.5 0.8,0.5 0.8,0.6" fill="black"></polygon>
+  </svg>
+</div>
---
+++ src/routes/svgTriangle/+page.svelte
@@ -0,0 +1,19 @@
+<style>
+  :global(body){
+    display:grid;
+    grid-template-columns: auto;
+    justify-content: center;
+    align-content: center;
+    height: 100vh;
+  }
+  div {
+    height: 200px;
+    width: 200px;
+  }
+</style>
+
+<div>
+  <svg viewBox="0 0 1 1" >
+    <polygon points="0,1 0.5,0 1,1" fill="black"></polygon>
+  </svg>
+</div>
